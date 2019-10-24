199db0ac8f617a0146fa75f9f3e56705c6886a6c
Jayrab95 <32508959+Jayrab95@users.noreply.github.com>
2019-10-24 16:32:20 +0200

    fixed namespaces

--- Grapholize_Prototype/MainWindow.xaml.cs
+++ Grapholize_Prototype/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Neosmartpen.Net;
 
 namespace Grapholize_Prototype
 {
